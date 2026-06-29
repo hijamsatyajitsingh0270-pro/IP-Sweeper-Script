5637d700614b888c22f6371ea634c3c23e1e295f
hijamsatyajitsingh0270-pro <hijamsatyajitsingh0270@gmail.com>
2026-06-29 19:05:30 +0530

    Add an update

--- IP_Sweeper_Script.sh
+++ IP_Sweeper_Script.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# Loop through IPs 1-254 in the subnet 
+for ip in {1..254}; do
+    # Ping each IP once
+    ping -c 1 $1.$ip | \
+    grep "64 bytes" | \
+    cut -d " " -f 4 | \
+    tr -d ":" &
+done
+
+#wait for all background processes to finished
+ wait
